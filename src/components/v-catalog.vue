656e24d0ffcf0b86002a46c800d20e6f6ba0286a
MontyPayton <urspectrans@gmail.com>
2022-09-20 15:31:00 +0500

    store

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,7 +2,7 @@
   <div class="v-catalog">
     
     <v-catalog-item 
-    v-for="produkt in produkts"
+    v-for="produkt in PRODUCTS"
     :key="produkt.article"
     :produkt_data="produkt"
     @article="showArticle"
@@ -12,7 +12,8 @@
 
 <script>
   import VCatalogItem from './v-catalog-item'
-import vCatalogItem from './v-catalog-item.vue'
+  import vCatalogItem from './v-catalog-item.vue'
+  import { mapActions, mapGetters } from 'vuex'
 export default {
   components: { VCatalogItem },
 comments:{
@@ -20,22 +21,34 @@ comments:{
 },
 data(){
   return{
-    title:"Каталог",
-    produkts:[
-      {image:"1.jpg",country:"Франция", name:"Гомельте",date:"15 октября",price:1100,article:"1"},
-      {image:"2.jpg",country:"Германия", name:"Друстфон",date:"16 октября",price:1150,article:"2"},
-      {image:"3.jpg",country:"Финляндия", name:"Маускань",date:"14 октября",price:1120,article:"3"},
-      {image:"4.jpg",country:"Россия", name:"Грязи",date:"18 октября",price:300,article:"4"},
-      {image:"5.jpg",country:"Беларусь", name:"Большие Грязи",date:"11 октября",price:500,article:"5"},
-      {image:"6.jpg",country:"Китай", name:"Яньхунь",date:"12 октября",price:700,article:"6"},
-    ]
+
+    // title:"Каталог",
+    // produkts:[
+    //   {image:"1.jpg",country:"Франция", name:"Гомельте",date:"15 октября",price:1100,article:"1"},
+    //   {image:"2.jpg",country:"Германия", name:"Друстфон",date:"16 октября",price:1150,article:"2"},
+    //   {image:"3.jpg",country:"Финляндия", name:"Маускань",date:"14 октября",price:1120,article:"3"},
+    //   {image:"4.jpg",country:"Россия", name:"Грязи",date:"18 октября",price:300,article:"4"},
+    //   {image:"5.jpg",country:"Беларусь", name:"Большие Грязи",date:"11 октября",price:500,article:"5"},
+    //   {image:"6.jpg",country:"Китай", name:"Яньхунь",date:"12 октября",price:700,article:"6"},
+    // ]
   }
 },
+computed:{
+  ...mapGetters([
+    "PRODUCTS"
+  ])
+},
 methods: {
+  ...mapActions([
+"GET_PRODUCTS_API"
+  ]),
   showArticle(data){
     console.log(data)
   }
 },
+mounted(){
+  this.GET_PRODUCTS_API()
+}
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,12 +6,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: []
   },
   getters: {
+    PRODUCTS(state){
+      return state.products;
+    }
   },
   mutations: {
+    SET_PRODUKTS_TO_STATE:(state ,products)=>{
+      state.products=products
+    }
+    
   },
   actions: {
+    GET_PRODUCTS_API({commit}){
+      return axios('http://localhost:3000/produkts',{
+        method:"GET"
+      })
+      .then((products) =>{
+        
+        commit('SET_PRODUKTS_TO_STATE', products.data)
+        return products;
+      })
+      .catch((error)=>{
+        console.log(error);
+        return error;
+      })
+    }
   },
   modules: {
   }
